79238e6baf7bc957cca2e31670dbb10cf8b41f64
Ismail Hasan <ismailhasan0132@gmail.com>
2017-08-29 23:34:22 -0700

    fixed his shit

--- hello world/src/helloworld.java
+++ hello world/src/helloworld.java
@@ -19,20 +19,27 @@ public class helloworld {
 			
 	
 	System.out.println(n + " people in the world have that same name.");
-	System.out.println("Are you a doctor, cop, fisherman, busdriver, or student?");
-
-	switch(in.nextLine()) {
-	
-	case "doctor":
-		System.out.println(x);
-
-	 default:
-		System.out.println("Okay, you didn't answer my question correctly, lets try again.");
-	
-	 
+	choice();
 		
+	 
 	
 }
+	
+	public static void choice() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Are you a doctor, cop, fisherman, busdriver, or student?");	
+		switch(in.nextLine()) {
+		
+//		case "doctor":
+//			System.out.println(x);
+
+		 default:
+			System.out.println("Okay, you didn't answer my question correctly, lets try again.");
+			choice();
+			break;
+			
+		
+	}
 	
 }
 }
