acfd6291bd10fa70cd70bf178a0e42c81cc929d3
Mamaoh <Mamaoh@DESKTOP-E3M3IJ3>
2017-08-30 22:47:56 -0700

    New project

---
+++ Main/src/Justin.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Justin {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("Hi! What's your name?");
+	name();
+	
+	
+	}
+	
+
+
+
+
+	public static void name() {
+		
+
+		
+		Scanner in = new Scanner(System.in);
+		
+		String name = in.nextLine();
+		System.out.println("Hi, " + name);
+		
+		cont1.cont();
+		
+		
+	}
+}
---
+++ Main/src/cont1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class cont1 {
+	
+	public cont1() {
+	
+	}
+	
+	public static void cont() {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Do you have a kik?");
+		
+		String name2 = in.nextLine();
+		System.out.println( name2 + "? Open bob and show vagene");
+}
+}
